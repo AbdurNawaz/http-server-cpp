8108a807c7224003162827c7805dda0c2ac9092a
Abdur Nawaz <abdurnawaz.2011@gmail.com>
2024-10-13 13:46:50 +0530

    added binding to port

--- src/server.cpp
+++ src/server.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <cstdlib>
+#include <string>
+#include <cstring>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 
 int main()
 {
-    std::cout << "Hello World" << std::endl;
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(4221);
+
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        std::cerr << "Failed to bind to port " << 4221 << std::endl;
+    }
+
     return 0;
 }
